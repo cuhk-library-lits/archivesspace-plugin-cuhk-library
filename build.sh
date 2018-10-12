44c5232d849d8938cee8bfeb604d8f140e3458c5
Alex Chan <alexchanwk@gmail.com>
2018-10-12 22:51:20 +0800

    Deploy for dev environment

--- build.sh
+++ build.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-PLUGIN_DIR="/d/bin/archivesspace/plugins"
 PLUGIN_NAME="cuhk-library-theme"
 VERSION="v2.0.0"
 
-OUTFILE_NAME="$PLUGIN_NAME-$VERSION.zip"
+ASPACE_ROOT="/d/bin/archivesspace"
+PLUGIN_DIR="$ASPACE_ROOT/plugins"
 
-git archive HEAD -o deployables/$OUTFILE_NAME
+trap ctrl_c INT
+function ctrl_c() {
+    exit
+}
+
+echo "Deploying..."
+if [[ "$1" == "dev" ]]
+then
+    OUTFILE_NAME="$PLUGIN_NAME-$VERSION-dev.zip"
+    DEV_STASH_NAME=`git stash create`
+    git archive $DEV_STASH_NAME -o deployables/$OUTFILE_NAME
+else
+    OUTFILE_NAME="$PLUGIN_NAME-$VERSION.zip"
+    git archive HEAD -o deployables/$OUTFILE_NAME
+fi
 rm -rf $PLUGIN_DIR/$PLUGIN_NAME
 mkdir $PLUGIN_DIR/$PLUGIN_NAME
 unzip deployables/$OUTFILE_NAME -d $PLUGIN_DIR/$PLUGIN_NAME
+
+echo "Starting server..."
+$ASPACE_ROOT/archivesspace-dev.bat
