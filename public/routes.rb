56dff03f6d0a30fe9ff5bc639364f0afca526393
Alex Chan <alexchanwk@gmail.com>
2018-10-29 21:34:47 +0800

    Fix incorrect url for site prefix

--- public/routes.rb
+++ public/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  post "repositories/:rid/resources/:id/pdf_uni"  => 'pdf_uni#resource'
+  scope AppConfig[:public_proxy_prefix] do
+    match('/repositories/:rid/resources/:id/pdf_uni' => 'pdf_uni#resource', :via => [:post])
+  end
 end
