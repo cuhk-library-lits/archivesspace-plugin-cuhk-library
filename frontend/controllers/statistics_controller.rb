8e022151c1c85a223cc353ea7f45635e04f0c6f9
Alex Chan <alexchanwk@gmail.com>
2019-02-25 18:50:49 +0800

    Statistics report per repository

--- frontend/controllers/statistics_controller.rb
+++ frontend/controllers/statistics_controller.rb
@@ -1,10 +1,9 @@
 class StatisticsController < ApplicationController
 
-  set_access_control  "view_repository" => [:index]
+  set_access_control  "view_repository" => [:show]
 
-  def index
-    @cat = {}
-    @cat["key"] = "value"
+  def show
+    @repository = JSONModel(:repository_with_agent).find(params[:repo_id])
   end
 
 end
--- frontend/routes.rb
+++ frontend/routes.rb
@@ -1,5 +1,5 @@
 ArchivesSpace::Application.routes.draw do
   scope AppConfig[:frontend_proxy_prefix] do
-    match('/plugins/statistics' => 'statistics#index', :via => [:get])
+    match('/plugins/statistics/:repo_id' => 'statistics#show', :via => [:get])
   end
 end
