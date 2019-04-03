c922e8d93c6cfdf047bef4c6e2a575f3032902ce
Alex Chan <alexchanwk@gmail.com>
2019-04-03 18:17:49 +0800

    Move statistics settings to "repository app configs"

--- frontend/controllers/statistics_controller.rb
+++ frontend/controllers/statistics_controller.rb
@@ -3,7 +3,14 @@ class StatisticsController < ApplicationController
   set_access_control  "view_repository" => [:show]
 
   def show
-    @repository = JSONModel(:repository_with_agent).find(params[:repo_id])
+    JSONModel.with_repository(params[:repo_id]) do
+        repo_app_configs = JSONModel(:repository_app_config).all
+        repo_app_configs.each do |app_config|
+          next unless app_config.config_key == ":repos_statistics"
+          repos_statistics = JSON.parse(app_config.config_json, :symbolize_names => true) unless app_config.config_json.nil?
+          @urls = repos_statistics[:urls]
+        end
+    end    
   end
 
 end
