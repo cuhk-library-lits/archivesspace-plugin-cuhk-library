f9a2a71867d9555c810e04aeffa301472d5831af
Alex Chan <alexchanwk@gmail.com>
2019-04-18 22:33:04 +0800

    Correct typo

--- public/controllers/reload_app_config_controller.rb
+++ public/controllers/reload_app_config_controller.rb
@@ -2,7 +2,7 @@ class ReloadAppConfigController < ApplicationController
 
   def reload
     AppConfigHelper.instance.reload
-    render :text => "AppConfig reladed at: " + Time.now.strftime("%Y-%m-%d %H:%M:%S") + " (#{Time.zone.to_s})"
+    render :text => "AppConfig reloaded at: " + Time.now.strftime("%Y-%m-%d %H:%M:%S") + " (#{Time.zone.to_s})"
   end
 
 end
